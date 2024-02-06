807c77a0106ddf2c1e37b7fff86defcbf7d453fa
OhitsAndrewG <andrew33gonzales@gmail.com>
2024-02-05 20:37:46 -0800

    Template

---
+++ PlantPants/PlantPants/PlantPantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantPantsApp.swift
+//  PlantPants
+//
+//  Created by Andrew Julian Gonzales on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PlantPantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
