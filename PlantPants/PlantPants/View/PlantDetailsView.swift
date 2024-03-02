63f31878a116d60c630c26674d2f75fa03f371b4
Ezequiel Reyes <reyeseze@usc.edu>
2024-03-01 18:29:25 -0800

    Plant details page for information

---
+++ PlantPants/PlantPants/View/PlantDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  PlantDetailsView.swift
+//  PlantPants
+//
+//  Created by Zeke Reyes on 2/26/24.
+//
+
+import SwiftUI
+
+struct PlantDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlantDetailsView()
+}
