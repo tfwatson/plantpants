030fbbbaee7db57b78f225003f2b62ec68d5326e
Andrew Gonzales <andrew33gonzales@gmail.com>
2024-04-18 18:44:57 -0700

    Delete .DS_Store

---
+++ plantpants/plantpants/plantpantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantpantsApp.swift
+//  plantpants
+//
+//  Created by Andrew Julian Gonzales on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct plantpantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
