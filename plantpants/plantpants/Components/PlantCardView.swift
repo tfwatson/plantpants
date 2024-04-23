8184d28697883146ead910f06e9f2970c022f351
Andrew Gonzales <andrew33gonzales@gmail.com>
2024-04-22 18:54:31 -0700

    Changed backgroundColor

--- plantpants/plantpants/Components/PlantCardView.swift
+++ plantpants/plantpants/Components/PlantCardView.swift
@@ -30,7 +30,7 @@ struct PlantCardView: View {
         .frame(width: 185, height: 100)
         .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
         .dynamicTypeSize(.accessibility3)
-        .background(Color("Flame"))
+        .background(Color("backgroundColor"))
         .cornerRadius(30)
         
     }
@@ -84,7 +84,7 @@ struct PalCardView: View {
         .frame(width: 185, height: 100)
         .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
         .dynamicTypeSize(.accessibility3)
-        .background(Color("Flame"))
+        .background(Color("backgroundColor"))
         .cornerRadius(30)
         
     }
